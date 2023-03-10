cfabc014541dc1acca99e693eb35dc0798a2ca16
Wilkens Joseph <jsw3430a@ens.fsi>
2023-03-10 15:42:49 +0100

    fin tp2

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,5 +1,9 @@
 package controleur;
 
+
+
+import personnages.Gaulois;
+import villagegaulois.Etal;
 import villagegaulois.Village;
 
 public class ControlAcheterProduit {
@@ -16,4 +20,47 @@ public class ControlAcheterProduit {
 	}
 
 	//TODO a completer
+	public boolean verifierIdentiteAcheteur(String nomAcheteur) {
+		return controlVerifierIdentite.verifierIdentite(nomAcheteur);
+	}
+	
+	public String[] trouverVendeurProduit(String nomProduit) {
+		Gaulois[] vendeurs= village.rechercherVendeursProduit(nomProduit);
+		String[] nomVendeurs= new String[vendeurs.length];
+		for(int i=0; i<vendeurs.length;i++) {
+			nomVendeurs[i]=vendeurs[i].getNom();
+		}
+		return nomVendeurs;
+	}
+	
+	public Etal[] trouverEtalsproduit(String[] nomVendeurs) {
+		Etal[] etals= new Etal[nomVendeurs.length];
+		for(int i=0; i<nomVendeurs.length;i++) {
+			etals[i]=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeurs[i]);
+		}
+		
+		return etals;
+	}
+	
+	public String[] donnerInfosproduit(String nomProduit) {
+		String[] nomVendeurs= trouverVendeurProduit(nomProduit);
+		Etal[] etals=trouverEtalsproduit(nomVendeurs);
+		int tailleTableau = etals.length;
+		String[] donnees = new String[tailleTableau];
+		for (int i = 0; i < etals.length; i++) {
+			Etal etal = etals[i];
+			Gaulois vendeur = etal.getVendeur();
+			donnees[i] = vendeur.getNom();
+			
+		}
+		return donnees;
+	}
+	
+	
+	public int acheterProduit(String nomVendeur, int quantite) {
+		Etal etal=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		return etal.acheterProduit(quantite);
+	}
+	
+	
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -14,5 +14,30 @@ public class BoundaryAcheterProduit {
 
 	public void acheterProduit(String nomAcheteur) {
 		// TODO à completer
+		
+		if (!controlAcheterProduit.verifierIdentiteAcheteur(nomAcheteur)) {
+			System.out.println("Il faut etre un habitant du village pour pouvoir acheter dans le marche");
+		}else {
+			System.out.println("Quel produit voulez-vous acheter?");
+			String produit=scan.nextLine();
+			String[] nomVendeur=controlAcheterProduit.donnerInfosproduit(produit);
+			if (nomVendeur!=null) {
+				System.out.println(nomAcheteur+",vous trouverez au marche:");
+				for (int i = 0; i < (nomVendeur.length);i++) {
+					System.out.println(i+" "+nomVendeur[i]);
+					}
+				int indice=Clavier.entrerEntier("Chez quel commercant voulez-vous acheter des "+produit);
+				String vendeur=nomVendeur[indice-1];
+				
+				System.out.println(nomAcheteur+" se déplace jusqu'à l'étal du vendeur "+vendeur);
+				
+				int nbproduit=Clavier.entrerEntier("Bonjour "+nomAcheteur+" Combien de fleurs voulez-vous acheter ?");
+				
+			} else {
+				System.out.println("Il n'y a aucun vendeur qui propose ce produit");
+			}
+			
+		}
+		
 	}
 }
