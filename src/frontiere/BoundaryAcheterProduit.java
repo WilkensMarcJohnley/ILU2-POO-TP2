16ecbe58b10033ea0d6343b6b323f17d07f2f008
WilkensMarcJohnley <josephwilkensmarcjohnley@outlook.com>
2023-03-23 12:09:50 +0100

    Fin TP3

--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -24,17 +24,27 @@ public class BoundaryAcheterProduit {
 			if (nomVendeur!=null) {
 				System.out.println(nomAcheteur+",vous trouverez au marche:");
 				for (int i = 0; i < (nomVendeur.length);i++) {
-					System.out.println(i+" "+nomVendeur[i]);
+					System.out.println((i+1)+"-"+nomVendeur[i]);
 					}
 				int indice=Clavier.entrerEntier("Chez quel commercant voulez-vous acheter des "+produit);
 				String vendeur=nomVendeur[indice-1];
 				
 				System.out.println(nomAcheteur+" se déplace jusqu'à l'étal du vendeur "+vendeur);
 				
-				int nbproduit=Clavier.entrerEntier("Bonjour "+nomAcheteur+" Combien de fleurs voulez-vous acheter ?");
+				int nbproduit=Clavier.entrerEntier("Bonjour "+nomAcheteur+"\nCombien de "+produit+" voulez-vous acheter ?");
+				
+				int prod_acheter=controlAcheterProduit.acheterProduit(vendeur, nbproduit);
+				if (prod_acheter==0) {
+					System.out.println(nomAcheteur+ " veut acheter "+ nbproduit+ " "+ produit+ ", malheureusement il n’y en a plus !");
+				}else if(prod_acheter<nbproduit) {
+					System.out.println(nomAcheteur+" veut acheter "+ nbproduit+" "+ produit+ ", malheureusement " + vendeur+ " n’en a plus que 3.\n" +nomAcheteur+" achète tout le stock de "+vendeur+".");
+				}else {
+					System.out.println(nomAcheteur+" achete "+ prod_acheter+" "+ produit+" a "+vendeur);
+				}
+				
 				
 			} else {
-				System.out.println("Il n'y a aucun vendeur qui propose ce produit");
+				System.out.println("Désolé, personne ne vend ce produit au marché.");
 			}
 			
 		}
